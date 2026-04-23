454ae46300b81ca4b7d47388b1a63ee136c12ddb
wirnty <skedovichusjdj@gmail.com>
2026-04-23 16:24:22 +0300

    Migrate MainActivity to Jetpack Compose
    Refactor MainActivity to use Jetpack Compose and update the button click action.

--- app/src/main/java/com.example.whiteapp/MainActivity.kt
+++ app/src/main/java/com.example.whiteapp/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.templateapk
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import coil.compose.AsyncImage
 
-class MainActivity : AppCompatActivity() {
-     Button(onClick = { 
-    // Code to execute when clicked
-        AsyncImage(
-            model = "https://t3.ftcdn.net/jpg/01/84/15/84/360_F_184158416_TslXPfNdz4HsMoiK9kWziHaqvJaDkBZ4.jpg",
-            contentDescription = "toast"
-)
-}) {
-    Text("click me plz")
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MyScreen()
+        }
+    }
+}
+
+@Composable
+fun MyScreen() {
+    Button(onClick = {
+        // действие при клике
+    }) {
+        Text("click me plz")
+    }
+
+    AsyncImage(
+        model = "https://t3.ftcdn.net/jpg/01/84/15/84/360_F_184158416_TslXPfNdz4HsMoiK9kWziHaqvJaDkBZ4.jpg",
+        contentDescription = "toast"
+    )
 }
